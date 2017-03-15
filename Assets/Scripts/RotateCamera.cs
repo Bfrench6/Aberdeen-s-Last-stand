d3600b3418ef92a4d2a7f09a5292ff9792b2a159
Evan <evanyui@hotmail.com>
2017-03-15 05:03:48 -0400

    added camera script and dummy level

---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour {
+
+	public GameObject target;
+	[SerializeField] private float speed = 2f;
+	[SerializeField] private int degree = 10;
+	private Vector3 point;
+
+	void Start () {
+		point = target.transform.position;
+		transform.LookAt(point);
+	}
+
+	void Update () {
+		//rotating around its Y axis, theta degrees per second times the speed
+		transform.RotateAround (point, Vector3.up, degree * Time.deltaTime * speed);
+	}
+}
