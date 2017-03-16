428e7c75bb837acd90adf83e21f4a0a9308ebc60
Bfrench6 <bfrench289100@gmail.com>
2017-03-16 03:42:06 -0400

    Added Basic Pause menu/Credits
    Added a basic pause menu with some functionality, more to come once more
    of the game is implemented. Also made a credits canvas with a script
    that will read text from a file and scroll up. Finally, added a blur
    effect to the camera

---
+++ Assets/CreditsScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class CreditsScript : MonoBehaviour
+{
+    public Text creditTextItem;
+    public Canvas CreditsCanvas;
+    private List<Text> Credits = new List<Text>();
+    private TextReader tr;
+    public string path;
+    private List<string> credits = new List<string>();
+    public float speed = 20.2f;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        // Set the path for the credits.txt file
+        path = "Assets/Resources/Credits.txt";
+        // Create reader & open file
+        tr = new StreamReader(path);
+        string temp;
+        while ((temp = tr.ReadLine()) != null)
+        {
+            // Read a line of text
+            credits.Add(temp);
+        }
+        // Close the stream
+        tr.Close();
+        CreateCredits();
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Credits.Count != 0)
+        {
+            for (int i = 0;i < Credits.Count; i++)
+            {
+                Credits[i].transform.Translate(Vector3.up * Time.deltaTime * speed);
+                
+            }
+        }
+    }
+    void CreateCredits()
+    {
+        for (int i = 0; i < credits.Count; i++)
+        {
+            string c = credits[i];
+            Text newTextItem = Instantiate(creditTextItem);
+            newTextItem.text = c;
+            newTextItem.transform.Translate(Vector3.down * i * newTextItem.preferredHeight*2);
+            Debug.Log(c);
+            Credits.Add(newTextItem);
+            newTextItem.transform.SetParent(CreditsCanvas.transform, false);
+
+        }
+    }
+
+}
---
+++ Assets/PauseMenuHandler.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using System;
+
+public class PauseMenuHandler : MonoBehaviour {
+
+    public Slider masterVolume;
+    public Slider musicVolume;
+    public Slider fxVolume;
+    public Button difficultyButton;
+	// Use this for initialization
+	void Start () {
+        masterVolume.onValueChanged.AddListener(delegate { ChangeMasterVolume(); });
+        musicVolume.onValueChanged.AddListener(delegate { ChangeMusicVolume(); });
+        fxVolume.onValueChanged.AddListener(delegate { ChangeFXVolume(); });
+
+        difficultyButton.onClick.AddListener(ChangeDifficulty);
+
+
+
+    }
+
+    void ChangeMasterVolume()
+    {
+        //TODO: change global master volume variable
+    }
+
+    void ChangeMusicVolume()
+    {
+        //TODO: change global music volume variable
+    }
+
+    void ChangeFXVolume()
+    {
+        //TODO: change global fx volume variable
+    }
+
+    void ChangeDifficulty()
+    {
+        Text diffTextElement = difficultyButton.GetComponentInChildren<Text>();
+        String diffText = diffTextElement.text;
+        switch (diffText)
+        {
+            case "Easy":
+                diffTextElement.text = "Medium";
+                //TODO: set global difficulty variable
+                break;
+            case "Medium":
+                diffTextElement.text = "Hard";
+                //TODO: set global difficulty variable 
+                break;
+            case "Hard":
+                diffTextElement.text = "Easy";
+                //TODO: set global difficulty variable
+                break;
+        }
+            
+
+    }
+
+
+
+}
