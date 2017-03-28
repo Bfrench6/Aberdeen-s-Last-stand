d3ecedde944be66144beacea7c68353cd09ad7f9
Pranav Bhardwaj <pranavbhard@gmail.com>
2017-03-28 16:10:06 -0400

    Uploaded level01 scripts
    Scripts to close canvas and spawn enemies upon closing

---
+++ Assets/CloseCanvas.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CloseCanvas : MonoBehaviour {
+
+    public bool close;
+    public GameObject canvas;
+    public Button button;
+    EnemySpawn script;
+    GameObject[] enemies;
+
+    // Use this for initialization
+    void Start () {
+        
+        close = false;
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        
+        canvas.SetActive(!close);
+        button.onClick.AddListener(TaskOnClick);
+
+    }
+
+    void TaskOnClick(){
+        close = true;
+        for (int i=0; i<enemies.Length; i++) {
+            EnemySpawn script = enemies[i].GetComponent<EnemySpawn>();
+            script.start = true;
+        }
+        
+    }
+
+}
---
+++ Assets/EnemySpawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour {
+
+    //CloseCanvas script;
+    public bool start;
+    bool started;
+    //Vector3 position;
+    Vector3 spawn;
+
+	// Use this for initialization
+	void Start () {
+		
+        start = false;
+        started = false;
+        //position = transform.position;
+        spawn = new Vector3(0, 5f, 0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (start  && !started) {
+            transform.position += spawn;
+            started = true;
+        }
+
+		
+	}
+}
